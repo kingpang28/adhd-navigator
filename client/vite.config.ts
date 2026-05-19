721e1ec2fbc67d802c1a4a0f97841df73dec0875
kingpang28 <141571499+kingpang28@users.noreply.github.com>
2026-05-19 22:38:12 +0100

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,9 @@
-import {jsxLocPlugin} from "@builder.io/vite-plugin-jsx-loc";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), jsxLocPlugin()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
